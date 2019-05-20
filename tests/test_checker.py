5bbd9dc543f14978e9b21c577a576cbeeacee5d5
fomko <ppopov92@gmail.com>
2019-05-21 02:06:46 +0300

    Added 2 tests

--- tests/test_checker.py
+++ tests/test_checker.py
@@ -1,16 +1,14 @@
+import os
+import shutil
 import pytest
-import sys
-from os import path
-sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
-
-from checker import check_rotated_file_size_param
-
+from checker import check_rotated_file_size_param,check_input_param
+from unittest import mock
 
 @pytest.mark.parametrize('param', [
-    {   #param0
+    {   # param0
         'file_size': "10GE"
     },
-    {   #param1
+    {   # param1
         'file_size': 10.12
     }
 ])
@@ -21,11 +19,11 @@ def test_invalid_check_rotated_file_size_param(param):
 
 
 @pytest.mark.parametrize("param", [
-    {   #param0
+    {   # param0
         'file_size': "10MB",
         'expected': 10485760
     },
-    {   #param1
+    {   # param1
         'file_size': 100000,
         'expected': 100000
     }
@@ -34,3 +32,28 @@ def test_valid_check_rotated_file_size_param(param):
     file_size = param['file_size']
     expected = param['expected']
     assert check_rotated_file_size_param(file_size) == expected
+
+# test with mock
+@mock.patch("checker.os")
+def test_valid_file_check_input_param(mock_os):
+    mock_os.path.exist.return_value = True
+    mock_os.path.isfile.return_value = True
+    mock_os.path.isdir.return_value = False
+    mock_os.path.abspath.return_value = "blabla"
+    assert check_input_param("blabla") == ["blabla"]
+
+
+# test with real file system!
+@pytest.fixture
+def make_files_for_test():
+    os.mkdir("temp")
+    log_path = "temp/test1.log"
+    with open(log_path, 'x') as t:
+        t.write("123")
+    yield make_files_for_test
+    shutil.rmtree("temp")
+
+
+def test_valid_dir_check_input_param(make_files_for_test):
+    expected = [os.path.join(os.getcwd(), "temp", "test1.log")]
+    assert check_input_param("temp") == expected
