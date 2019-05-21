7b51de5200a3bba82c8d6d4a095fc0327ffb883e
fomko <fomko@users.noreply.github.com>
2019-05-21 15:09:29 +0300

    Hot fix for test with mock

--- tests/test_checker.py
+++ tests/test_checker.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 import pytest
-from checker import check_rotated_file_size_param,check_input_param
+from .. import checker
 from unittest import mock
 
 @pytest.mark.parametrize('param', [
@@ -15,7 +15,7 @@ from unittest import mock
 def test_invalid_check_rotated_file_size_param(param):
     file_size = param['file_size']
     with pytest.raises(SystemExit):
-        check_rotated_file_size_param(file_size)
+        checker.check_rotated_file_size_param(file_size)
 
 
 @pytest.mark.parametrize("param", [
@@ -31,16 +31,16 @@ def test_invalid_check_rotated_file_size_param(param):
 def test_valid_check_rotated_file_size_param(param):
     file_size = param['file_size']
     expected = param['expected']
-    assert check_rotated_file_size_param(file_size) == expected
+    assert checker.check_rotated_file_size_param(file_size) == expected
 
 # test with mock
-@mock.patch("checker.os")
+@mock.patch("os.path")
 def test_valid_file_check_input_param(mock_os):
-    mock_os.path.exist.return_value = True
-    mock_os.path.isfile.return_value = True
-    mock_os.path.isdir.return_value = False
-    mock_os.path.abspath.return_value = "blabla"
-    assert check_input_param("blabla") == ["blabla"]
+    mock_os.exist.return_value = True
+    mock_os.isfile.return_value = True
+    mock_os.isdir.return_value = False
+    mock_os.abspath.return_value = "blabla"
+    assert checker.check_input_param("blabla") == ["blabla"]
 
 
 # test with real file system!
@@ -56,4 +56,4 @@ def make_files_for_test():
 
 def test_valid_dir_check_input_param(make_files_for_test):
     expected = [os.path.join(os.getcwd(), "temp", "test1.log")]
-    assert check_input_param("temp") == expected
+    assert checker.check_input_param("temp") == expected
